2380dbb9cd0e5a5909d643cbfd793bb72e2e8332
cobeta <b.cobeta@gmail.com>
2025-08-15 13:28:35 -0400

    finished exercise

--- makeText.js
+++ makeText.js
@@ -1,2 +1,51 @@
-/** Command-line tool to generate Markov text. */
+/** Command-line tool to generate Markov text. 
 
+$node makeText.js file eggs.txt
+... generated text from file 'eggs.txt' ...
+
+$node makeText.js url http://www.gutenberg.org/files/11/11-0.txt
+... generated text from that URL ...*/
+
+const fs = require('fs');
+const axios = require('axios');
+const markov = require("./markov");
+
+const makeTextFromText = (sourceText) => {
+    const model = new markov.MarkovMachine(sourceText);
+    
+    console.log(model.makeText());
+}
+
+const makeTextFromFile = (path) => {
+    try {
+        const content = fs.readFileSync(path, 'utf8');
+        makeTextFromText(content);
+    } catch (e) {
+        console.log(`Error reading file:\n  ${e}`);
+    }
+}
+
+const makeTextFromURL = async (URL) => {
+    try {
+        const response = await axios.get(URL);
+        const content = response.data;
+        makeTextFromText(content);
+    } catch (e) {
+        return `Error fetching ${URL}:\n  ${e.message}`;
+    }
+}
+
+// Read 
+let [source, path] = process.argv.slice(2);
+
+console.log(source);
+console.log(path);
+
+if (source === "file") {
+  makeTextFromFile(path);
+} else if (source === "url") {
+  makeTextFromURL(path);
+} else {
+  console.error(`Unknown source type: ${source}`);
+  process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -17,13 +17,60 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = {};
+
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (!chains[word]) {
+        chains[word] = [];
+      }
+      chains[word].push(nextWord);
+    }
+
+    this.chains = chains;
   }
 
 
   /** return random text from chains */
+  getNextWord(lastWord) {
+    const nextWords = this.chains[lastWord];
+    if (!nextWords || nextWords.length === 0) return null;
+    const randIdx = Math.floor(Math.random() * nextWords.length);
+    return nextWords[randIdx];
+  }
+
 
   makeText(numWords = 100) {
-    // TODO
+    const chainKeys = Object.keys(this.chains);
+
+    let lastWord;
+    do {
+      lastWord = chainKeys[Math.floor(Math.random() * chainKeys.length)];
+    } while (!/^[A-Z]/.test(lastWord));
+
+    let output = lastWord;
+    let finished = false;
+
+    while (!finished) {
+      lastWord = this.getNextWord(lastWord);
+
+      const wordCountReached = output.split(/\s+/).length >= numWords;
+      const lastWordIsNull = lastWord === null;
+      const lastWordEndsWithPeriod = typeof lastWord === "string" && lastWord.endsWith('.');
+
+      finished = wordCountReached && (lastWordIsNull || lastWordEndsWithPeriod);
+
+      output += ' ' + lastWord;
+      
+    }
+
+    return output;
+  
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
